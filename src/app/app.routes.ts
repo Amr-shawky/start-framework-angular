3cbaa1cb701715f018bde46923ec9719b32aa683
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-01 14:19:52 +0300

    feat: add routing and components for Home, About, Contact, and Portfolio
    - Implemented routing in app.routes.ts for Home, About, Contact, and Portfolio components.
    - Created Home, About, Contact, Portfolio components with respective HTML and CSS files.
    - Added Navbar and Footer components to app.ts.
    - Removed unused Contact and Home components and their associated files.
    - Added basic structure for Navbar and Footer components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/home/home';
+import { About} from './component/about/about';
+import { Contact } from './component/contact/contact';
+import { Portfolio } from './component/portfolio/portfolio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+    { path: 'portfolio', component: Portfolio }
+];
