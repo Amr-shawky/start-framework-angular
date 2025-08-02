33da97a5818092c8a4eb1b845791c53da05d86dc
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-02 12:31:37 +0300

    feat: integrate FontAwesome icons and enhance home and navbar components
    - Added FontAwesome Angular library and free solid icons to package.json and package-lock.json.
    - Updated home component to include a header with an icon and styled text.
    - Created home.css for custom styles including a primary color variable and header styling.
    - Enhanced navbar with a new design, including brand name and navigation links.
    - Added home-icon.svg for use in the home component.

--- src/app/component/home/home.ts
+++ src/app/component/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  faCoffee = faCoffee;
+  faStar = faStar;
 
 }
