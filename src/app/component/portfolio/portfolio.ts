49c5269e0311926e98041480b77a07d79d6b8558
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-03 00:02:31 +0300

    feat: enhance portfolio component layout and functionality with image click handling

--- src/app/component/portfolio/portfolio.ts
+++ src/app/component/portfolio/portfolio.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-portfolio',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './portfolio.html',
-  styleUrl: './portfolio.css'
+  styleUrl: './portfolio.css',
 })
 export class Portfolio {
-  faStar=faStar;
+  faStar = faStar;
+  currentImageSrc: string = '';
+  closeImage() {
+    this.currentImageSrc = '';
+  }
+
+  openImage(src: string) {
+    this.currentImageSrc = src;
+  }
 }
