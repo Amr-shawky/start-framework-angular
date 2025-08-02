2a8e4df73a68a58aed9e30a659ac748c18b8a498
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-02 20:21:07 +0300

    feat: add FontAwesome brand icons and update footer component layout

--- src/app/component/footer/footer.ts
+++ src/app/component/footer/footer.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [ FontAwesomeModule,  ],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class Footer {
-
+faFacebook = faFacebook;
+faTwitter = faTwitter;
+faLinkedinIn = faLinkedinIn;
+faGlobe = faGlobe;
 }
