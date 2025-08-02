31572d11f63d91ec66537bba7a23bda1210639d6
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-02 21:42:57 +0300

    Refactor code structure for improved readability and maintainability

--- src/app/component/portfolio/portfolio.ts
+++ src/app/component/portfolio/portfolio.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.css'
 })
 export class Portfolio {
-
+  faStar=faStar;
 }
