9f63371b8f10ebff494eb5be02adf92e0bbb1670
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-02 20:57:11 +0300

    feat: implement About component with FontAwesome star icon and enhance navbar links

--- src/app/component/about/about.ts
+++ src/app/component/about/about.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
-
+faStar = faStar;
 }
